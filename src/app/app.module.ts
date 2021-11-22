f3a10134348fdab5f8b79bc24f22cbfd5460480a
benitezalexis <abenitezcolman96@gmail.com>
2021-11-21 22:46:39 -0300

    Create components and routes
    The components about me, projects, create projects and contacts with their respective routes were created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {routing, appRoutingProviders} from './app.routing';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProyectsComponent,
+    CreateComponent,
+    ContactComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    routing
   ],
-  providers: [],
+  providers: [
+  appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
