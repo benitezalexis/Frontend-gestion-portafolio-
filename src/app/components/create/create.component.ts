9b4c4eb1856e9382cb6fb7d744bbb42b2e4a8216
benitezalexis <abenitezcolman96@gmail.com>
2021-11-28 17:49:40 -0300

    Save new projects to portfolio

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -11,12 +11,14 @@ export class CreateComponent implements OnInit {
 
     public title: string;
     public project: Project;
+    public status: string;
 
     constructor(
         private _projectService: ProjectService
       ) {
           this.title="Crear proyecto";
           this.project=new Project('','','','',2021,'','');   
+          this.status=""
         }
 
     ngOnInit(): void {
@@ -24,7 +26,23 @@ export class CreateComponent implements OnInit {
 
 
    onSubmit(form:any) {
-      console.log(this.project);
-   }
+    
+      this._projectService.saveProject(this.project).subscribe(
+            response=>{
+                    if(response.project){
+                        this.status='success';
+                        form.reset();
+                    }else{
+                        this.status='failed';
+                    }
+                },
+            error=>{
+                console.log(<any>error);
+            }
+
+
+        );
+
+    }
 
 }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -18,4 +18,11 @@ export class ProjectService{
 		testService(){
 			return "PRobando el servcio de angular";
 		}
+
+		saveProject(project: Project):Observable<any>{
+			let params=JSON.stringify(project);
+			let headers=new HttpHeaders().set('Content-Type','application/json');
+			return this._http.post(this.url+'save-project',params,{headers:headers});
+
+		}
 }
