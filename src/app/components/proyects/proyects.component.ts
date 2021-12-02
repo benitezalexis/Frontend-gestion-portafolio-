82f9c98b5ee7c94b12d3bc1c7ff24af5c702b9db
benitezalexis <abenitezcolman96@gmail.com>
2021-12-02 08:41:35 -0300

    add styles to the project list

--- src/app/components/proyects/proyects.component.ts
+++ src/app/components/proyects/proyects.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import { ProjectService} from '../../services/project.service';
+import { Global } from '../../services/global';
 
 @Component({
   selector: 'app-proyects',
   templateUrl: './proyects.component.html',
-  styleUrls: ['./proyects.component.css']
+  styleUrls: ['./proyects.component.css'],
+  providers: [ProjectService]
 })
 export class ProyectsComponent implements OnInit {
 
-  constructor() { }
+
+  public projects!:Project[];
+  public url:string;
+
+  constructor(
+      private _projectService: ProjectService
+    ) { 
+        this.url=Global.url;
+
+      }
 
   ngOnInit(): void {
+    this.getProjects();
+  }
+
+  getProjects(){
+    this._projectService.getProjects().subscribe(
+        response=>{
+          if(response.projectss){
+            this.projects=response.projectss;
+            console.log("hola");  
+          }
+          console.log(response);
+        },
+        error=>{
+          console.log(<any>error);
+        }
+
+      );
   }
 
 }
